b5d4509b57c0f78d6e79373d85780bf0454d3911
Matthew Pfeiffer <spferical@gmail.com>
2020-03-02 22:50:47 -0500

    Generate char class descriptions

--- chargen/main.py
+++ chargen/main.py
@@ -1,7 +1,50 @@
 #!/usr/bin/env python3
 from enum import Enum
+import random
+import logging
 
 import urwid
+
+
+logging.basicConfig(filename="chargen.log", level=logging.DEBUG)
+
+
+class CHAR_CLASSES(Enum):
+    FIGHTING_MAN = "Fighting Man"
+    MAGIC_USER = "Magic User"
+    CLERIC = "Cleric"
+
+
+CHAR_CLASS_DESC_FRAGMENTS = {
+    CHAR_CLASSES.FIGHTING_MAN: [
+        "Fight.",
+        "Hit the bad guys.",
+        "Swing the sharp thing around.",
+        "Real tough.",
+        "Strong.",
+        "Big muscles.",
+        "Huge muscles.",
+        "Hold a big stick.",
+    ],
+    CHAR_CLASSES.MAGIC_USER: [
+        "Eat manna.",
+        "Abracadabra.",
+        "Big pointy hat.",
+        "Zip zoop zap!",
+        "Stroke the beard.",
+        "You shall not pass.",
+        "Accurate missile targeting.",
+    ],
+    CHAR_CLASSES.CLERIC: [
+        "Pray.",
+        "Wololo.",
+        "Fanciest hat of them all.",
+        "Dear lord.",
+        "Get more HPs.",
+        "Not afraid of skeletons.",
+        "The protection racket has been nerfed.",
+    ],
+}
 
 
 class BetterButton(urwid.Button):
@@ -9,7 +52,7 @@ class BetterButton(urwid.Button):
     button_right = urwid.Text("")
 
 
-def menu(title, choices, display_fn=str):
+def menu(title, choices, display_fn=str, description_fn=lambda c: ""):
     selected = None
     body = [urwid.Text(title), urwid.Divider()]
 
@@ -22,8 +65,17 @@ def menu(title, choices, display_fn=str):
         button = BetterButton(display_fn(c))
         urwid.connect_signal(button, "click", item_chosen, c)
         body.append(urwid.AttrMap(button, None, focus_map="reversed"))
-    menu_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
-    right_txt = urwid.Text("foo")
+    menu_walker = urwid.SimpleFocusListWalker(body)
+    menu_widget = urwid.ListBox(menu_walker)
+    right_txt = urwid.Text("")
+
+    def on_focus_changed():
+        focused_index = menu_widget.get_focus()[1]
+        focused_choice = choices[focused_index - 2]
+        right_txt.set_text(description_fn(focused_choice))
+
+    urwid.connect_signal(menu_walker, "modified", on_focus_changed)
+
     right_fill = urwid.Filler(right_txt, valign="top")
     right_pad = urwid.Padding(right_fill, left=1, right=1)
     columns = urwid.Columns([menu_widget, right_pad])
@@ -40,14 +92,22 @@ def menu(title, choices, display_fn=str):
     return selected
 
 
-class CLASSES(Enum):
-    FIGHTING_MAN = "Fighting Man"
-    MAGIC_USER = "Magic User"
-    CLERIC = "Cleric"
+def char_class_desc(cclass):
+    text = ""
+    for _ in range(3):
+        text += random.choice(CHAR_CLASS_DESC_FRAGMENTS[cclass]) + " "
+    return text
 
 
 def choose_class():
-    print(menu("CHOOSE YOUR CLASS", list(CLASSES), display_fn=lambda c: c.value))
+    print(
+        menu(
+            "CHOOSE YOUR CLASS",
+            list(CHAR_CLASSES),
+            display_fn=lambda c: c.value,
+            description_fn=char_class_desc,
+        )
+    )
 
 
 def main():
