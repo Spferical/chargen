a1108c48c3c0dd8f95c32c7d6967aa6219ddd490
Srinivas Kaza <srinivas@kaza.io>
2020-03-07 18:57:09 -0500

    Fix formatting

--- chargen/main.py
+++ chargen/main.py
@@ -254,7 +254,8 @@ EVENTS = [
                 success=EventResult(
                     desc="It's fascinating.", stat_mods={STATS.INT: +2}
                 ),
-                failure=EventResult(desc="It's too hard to understand.", stat_mods={}),
+                failure=EventResult(
+                    desc="It's too hard to understand.", stat_mods={}),
             ),
             EventChoice(
                 name="Splash in puddles",
@@ -269,7 +270,8 @@ EVENTS = [
                 name="Conduct a sun ritual",
                 skill_reqs={},
                 checks=[StatCheck(STATS.WIS, 1, 20, 20)],
-                success=EventResult(desc="The rain slows.", stat_mods={STATS.WIS: +2},),
+                success=EventResult(desc="The rain slows.",
+                                    stat_mods={STATS.WIS: +2},),
                 failure=EventResult(
                     desc="Nothing happens.", stat_mods={STATS.WIS: +1},
                 ),
@@ -283,14 +285,17 @@ EVENTS = [
                 name="Eat the green crap.",
                 skill_reqs=[],
                 checks=[StatCheck(STATS.WIS, num_dice=1, sides=20, dc=20)],
-                success=EventResult(desc="You get it down.", stat_mods={STATS.CON: +2}),
-                failure=EventResult(desc="You spit it out.", stat_mods={STATS.CON: -1}),
+                success=EventResult(desc="You get it down.",
+                                    stat_mods={STATS.CON: +2}),
+                failure=EventResult(desc="You spit it out.",
+                                    stat_mods={STATS.CON: -1}),
             ),
             EventChoice(
                 name="Pretend to eat it.",
                 skill_reqs=[],
                 checks=[StatCheck(STATS.DEX, 1, 20, 22)],
-                success=EventResult(desc="", stat_mods={STATS.CON: -1, STATS.DEX: +2},),
+                success=EventResult(desc="", stat_mods={
+                                    STATS.CON: -1, STATS.DEX: +2},),
                 failure=EventResult(desc="", stat_mods={STATS.CON: -2},),
             ),
             EventChoice(
@@ -383,7 +388,8 @@ class PointBuy(urwid.WidgetWrap):
         ]
 
         def on_change(*args):
-            points_left_text.set_text(f"Points left: {self.get_points_remaining()}")
+            points_left_text.set_text(
+                f"Points left: {self.get_points_remaining()}")
 
         stat_edit_column = [urwid.Text("STATS")]
         stat_bonus_column = [urwid.Text("CLASS BONUSES")]
@@ -413,7 +419,8 @@ class PointBuy(urwid.WidgetWrap):
             self.warning_text.set_text("")
         if key in ("enter", " "):
             if self.get_points_remaining() != 0:
-                self.warning_text.set_text(("warn", "Must have zero points remaining."))
+                self.warning_text.set_text(
+                    ("warn", "Must have zero points remaining."))
                 return
             stats = {
                 stat: editor.value() + self.bonuses.get(stat, 0)
@@ -451,14 +458,17 @@ class PlayerDisplay(urwid.WidgetWrap):
             self.age_text.set_text(str(char_info.age))
         self.skill_pile.contents.clear()
         for skill in sorted(char_info.skills, key=lambda s: s.value):
-            self.skill_pile.contents.append((urwid.Text(skill.value), ("pack", None)))
+            self.skill_pile.contents.append(
+                (urwid.Text(skill.value), ("pack", None)))
 
 
 class Game:
     def __init__(self):
-        self.main_widget_container = urwid.Padding(urwid.Edit(), left=1, right=1)
+        self.main_widget_container = urwid.Padding(
+            urwid.Edit(), left=1, right=1)
         self.player_display = PlayerDisplay()
-        columns = urwid.Columns([self.main_widget_container, self.player_display])
+        columns = urwid.Columns(
+            [self.main_widget_container, self.player_display])
         padded = urwid.Padding(columns, left=2, right=2)
         overlay = urwid.Overlay(
             padded,
@@ -576,13 +586,15 @@ class Game:
         return self.make_popup(layout)
 
     def close_popup(self):
-        self.set_main_widget(self.main_widget_container.original_widget.bottom_w)
+        self.set_main_widget(
+            self.main_widget_container.original_widget.bottom_w)
 
     def roll_stat_check(self, stat, num_dice, sides):
         return self.player.stats[stat] + self.dice(num_dice, sides)
 
     def play_random_event(self):
-        events = list(filter((lambda e: e.desc not in self.seen_events), EVENTS))
+        events = list(
+            filter((lambda e: e.desc not in self.seen_events), EVENTS))
         if not events:
             logging.warning("Ran out of random events")
             return
