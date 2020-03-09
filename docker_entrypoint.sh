33421e5370cf5fae14adbdacb69da0f1c1c00e51
Matthew Pfeiffer <spferical@gmail.com>
2020-03-08 23:25:45 -0400

    Change game title

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 USER=chargen
 chown -R $USER /chargen && su - chargen -c \
-'cd /chargen && gotty -w --title-format 7DRL pipenv --bare run python3 chargen/main.py'
+'cd /chargen && gotty -w --title-format "Game of Centuries" pipenv --bare run python3 chargen/main.py'
